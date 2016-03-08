2e3c550a52c2b8ea5917f7b77f56ecd46671fc9f
Kahlin Baughman <baughmankahlin@gmail.com>
2016-03-08 10:19:40 -1000

    Add member variables and start on maze

--- src/Maze.java
+++ src/Maze.java
@@ -15,10 +15,49 @@
  * DESCRIPTION: This is the file that contains the methods for the Maze class
  *
  ***************************************************************************************/
+ import java.util.*;
+ import java.io.*;
+
 public class Maze{
+  int rows;
+  int cols;
 
 
 
+  /* Constructs the maze of a given size, using the given file.*/
+  public Maze(int numRows, int numCols, String filename){
+    rows = numRows;
+    cols = numCols;
+    Scanner fr = new Scanner(filename);
+    while(fr.hasNext()){
+      
+    }
 
+  }
 
+  /*Traverses through the maze using a Stack. If there is no start, print “This maze has no start!”
+  If an exit cannot be found then print, “No exit was found.” Otherwise, as soon as an exit has
+  been found, stop traversing through the maze, and print, “You have found the exit!” Each
+  time a cell is visited, print the maze.*/
+  public void findExitUsingStack(){
+
+  }
+
+  /*Traverses through the maze using a Queue. If there is no start, print “This maze has no start!”
+  If an exit cannot be found then print, “No exit was found.” Otherwise, as soon as an exit has
+  been found, stop traversing through the maze, and print, “You have found the exit!” Each
+  time a cell is visited, print the maze.*/
+  public void findExitUsingQueue(){
+
+  }
+
+  /*Resets the maze using the initial file.*/
+  public void resetMaze(){
+
+  }
+
+  /*Returns a String representation of the maze*/
+  public String toString(){
+
+  }
 }
